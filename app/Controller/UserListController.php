620b9787ef7998c4bf219d25075e35f4cfa3d1ee
Ramón Cahenzli <rca@psy-q.ch>
2017-11-16 16:06:37 +0100

    Moving user search to its own action

--- app/Controller/UserListController.php
+++ app/Controller/UserListController.php
@@ -17,15 +17,43 @@ class UserListController extends BaseController
      */
     public function show()
     {
+        $paginator = $this->userPagination->getListingPaginator();
+
+        $this->response->html($this->helper->layout->app('user_list/listing', array(
+            'title' => t('Users').' ('.$paginator->getTotal().')',
+            'paginator' => $paginator,
+        )));
+    }
+
+    /**
+     * Search in users
+     *
+     * @access public
+     */
+    public function search()
+    {
         $search = urldecode($this->request->getStringParam('search'));
         $paginator = $this->userPagination->getListingPaginator();
+
+        // TODO: Big mistery: How to create a $userFilter?
+
+        if ($search !== '' && ! $paginator->isEmpty()) {
+            $paginator
+                ->setQuery(
+                    $this->userModel
+                    ->getQuery()
+                    ->withFilter($userFilter)
+                )
+                ->calculate();
+        }
 
         $this->response->html($this->helper->layout->app('user_list/listing', array(
             'title' => t('Users').' ('.$paginator->getTotal().')',
             'values' => array(
                 'search' => $search,
             ),
-            'paginator' => $paginator,
+            'paginator' => $paginator
         )));
     }
+
 }
